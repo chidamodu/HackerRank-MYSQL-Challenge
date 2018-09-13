7a64af9e4721bc39441ec463a91807298ba766c3
Chidam Raji <chidambaram.spv@gmail.com>
2018-09-13 15:56:55 -0700

    Find Euclidean Distance in MySQL
    This commit finds the Euclidean Distance between points P1(a,c) and P2(b,d).

---
+++ Weather Observation Station 19 .sql
@@ -0,0 +1,7 @@
+Weather Observation Station 19
+
+Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where  (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and  (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+
+SELECT ROUND(SQRT(POW((MIN(LAT_N)-MAX(LAT_N)), 2)+POW((MIN(LONG_W)-MAX(LONG_W)), 2)), 4)
+FROM Station;
